76eb6dce59abe0a7e6787de060c6b6c92a1ad2b8
Jattin Manhas <jattinmanhas0102@gmail.com>
2022-05-17 13:31:03 +0530

    New Commit

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,13 +4,13 @@ import {getAuth} from 'firebase/auth'
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBr4wdrMZQY9EgmXybeUKRtDWiPxnEUCJA",
-  authDomain: "resume-maker-c35bd.firebaseapp.com",
-  projectId: "resume-maker-c35bd",
-  storageBucket: "resume-maker-c35bd.appspot.com",
-  messagingSenderId: "181859682589",
-  appId: "1:181859682589:web:1c27b1dea8a84a3e69b9f4",
-  measurementId: "G-68HQY7YW84"
+  apiKey: "AIzaSyC1DadIIQ7Srak0ejCemLf6i5n4kGFO7xg",
+  authDomain: "login-resume-system.firebaseapp.com",
+  projectId: "login-resume-system",
+  storageBucket: "login-resume-system.appspot.com",
+  messagingSenderId: "267060851838",
+  appId: "1:267060851838:web:bd5f2d9923a1fb16fa142f",
+  measurementId: "G-3D7E36HFCL"
 };
 
 // Initialize Firebase
